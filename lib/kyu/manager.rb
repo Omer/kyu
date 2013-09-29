7a4761cfc4cfe9901ee55b87099189ac532ac6b4
Omer Jakobinsky <omer@jakobinsky.com>
2013-09-29 11:13:06 +0100

    Encapsulate options in the Worker instead of the Manager
    This enables marginally better API.

--- lib/kyu/manager.rb
+++ lib/kyu/manager.rb
@@ -1,28 +1,22 @@
 module Kyu
   class Manager
-    attr_reader :worker_klass, :queue, :dl_queue, :max_retries, :logger,
-      :threadpool_size, :error_callback
+    attr_reader :worker, :queue, :dl_queue
 
-    def initialize( worker_klass, queue_name, options={} )
-      @max_retries = options.fetch( :max_retries, 3 )
-      @threadpool_size = options.fetch( :threadpool_size, 20 )
-      @logger = options.fetch( :logger, Logger.new( '/dev/null' ) )
-      @error_callback = options.fetch( :error_callback, ->( err ){} )
-      queue_options = options.fetch( :queue_options, {} )
-      @worker_klass = worker_klass
-      @sqs = AWS::SQS.new
-      @queue = @sqs.queues.create( queue_name, queue_options )
-      @dl_queue = @sqs.queues.create( deadletter_queue_name_for( queue_name ), queue_options )
+    def initialize( worker_klass, queue_name )
+      @worker = worker_klass.new
+      sqs = AWS::SQS.new
+      @queue = sqs.queues.create( queue_name, worker.queue_options )
+      @dl_queue = sqs.queues.create( deadletter_queue_name_for( queue_name ), worker.queue_options )
     end
 
     def start
-      logger.info( "Started listening for messages on: '#{queue.arn}'" )
-      logger.info(
+      worker.logger.info( "Started listening for messages on: '#{queue.arn}'" )
+      worker.logger.info(
         "Messages that could not be processes would be imgrated to: '#{dl_queue.arn}'"
       )
 
       EM.run do
-        EM.threadpool_size = threadpool_size
+        EM.threadpool_size = worker.threadpool_size
         stop = false
 
         Signal.trap( 'INT' ) { EM.stop; stop = true }
@@ -31,7 +25,7 @@ module Kyu
         poll_message until stop
       end
 
-      logger.info( "Stopped listening for messages on: '#{queue.arn}'" )
+      worker.logger.info( "Stopped listening for messages on: '#{queue.arn}'" )
     end
 
     private
@@ -42,15 +36,15 @@ module Kyu
 
       EM.defer do
         begin
-          logger.info( "Started processing: '#{msg.body}'" )
-          worker_klass.new.process_message( JSON.parse( msg.body ) )
+          worker.logger.info( "Started processing: '#{msg.body}'" )
+          worker.process_message( JSON.parse( msg.body ) )
           msg.delete
-          logger.info( "Finished processing: '#{msg.body}'" )
+          worker.logger.info( "Finished processing: '#{msg.body}'" )
         rescue => err
-          logger.error( stringify_exception( err ) )
-          error_callback.call( err )
+          worker.logger.error( stringify_exception( err ) )
+          worker.error_callback.call( err )
           if msg.receive_count > max_retries
-            logger.info( "Max number of reties exceeded for: '#{msg.body}'. Migrating the message to the dead-letter queue." )
+            worker.logger.info( "Max number of reties exceeded for: '#{msg.body}'. Migrating the message to the dead-letter queue." )
             dl_queue.send_message( msg.body )
             msg.delete
           end
--- lib/kyu/worker.rb
+++ lib/kyu/worker.rb
@@ -4,9 +4,19 @@ module Kyu
       base.extend( ClassMethods )
     end
 
+    def max_retries; 3; end
+
+    def threadpool_size; 5; end
+
+    def logger; Logger.new( '/dev/null' ); end
+
+    def error_callback; ->(err){}; end
+
+    def queue_options; {}; end
+
     module ClassMethods
-      def start( queue_name, options={} )
-        Manager.new( self, queue_name, options ).start
+      def start( queue_name )
+        Manager.new( self, queue_name ).start
       end
     end
   end
